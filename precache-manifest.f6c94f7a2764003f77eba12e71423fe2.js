838bfe06ce08b8dfb5e0ddcadc987501b0fc6b72
vnsteven <nvguyen@gmail.com>
2019-08-01 21:45:37 +0200

    Updates

---
+++ precache-manifest.f6c94f7a2764003f77eba12e71423fe2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ec9e5528c0d17cf8899dae8f39acc0",
+    "url": "/hacker-news-react/index.html"
+  },
+  {
+    "revision": "de1dcb00e984c33fbba4",
+    "url": "/hacker-news-react/static/css/2.35a696c3.chunk.css"
+  },
+  {
+    "revision": "2c05f5bc4b300118c4a8",
+    "url": "/hacker-news-react/static/css/main.02ca23b9.chunk.css"
+  },
+  {
+    "revision": "de1dcb00e984c33fbba4",
+    "url": "/hacker-news-react/static/js/2.a4422a9b.chunk.js"
+  },
+  {
+    "revision": "2c05f5bc4b300118c4a8",
+    "url": "/hacker-news-react/static/js/main.1a3bd6f9.chunk.js"
+  },
+  {
+    "revision": "95d28fe0f00ae3be17cb",
+    "url": "/hacker-news-react/static/js/runtime~main.efff1574.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hacker-news-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hacker-news-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hacker-news-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hacker-news-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hacker-news-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7e3f16ef799f4d8511b55846dc04c409",
+    "url": "/hacker-news-react/static/media/logo.7e3f16ef.png"
+  }
+]);
